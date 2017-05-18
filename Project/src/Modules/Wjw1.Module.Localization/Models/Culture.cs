966ef6fea479f92e6764249cf61b39937dcf7514
JuRogn <rongyou@msn.com>
2017-05-18 17:23:00 +0800

    本地化问题

---
+++ Project/src/Modules/Wjw1.Module.Localization/Models/Culture.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+using Wjw1.Infrastructure;
+
+namespace Wjw1.Module.Localization.Models
+{
+    public class Culture : DbSetBase
+    {
+        public string Name { get; set; }
+
+        public IList<Resource> Resources { get; set; }
+    }
+}
---
+++ Project/src/Modules/Wjw1.Module.Localization/Models/Resource.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+using Wjw1.Infrastructure;
+
+namespace Wjw1.Module.Localization.Models
+{
+    public class Resource : DbSetBase
+    {
+        public string Key { get; set; }
+
+        public string Value { get; set; }
+
+        public long CultureId { get; set; }
+
+        public Culture Culture { get; set; }
+    }
+}
